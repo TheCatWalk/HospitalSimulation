651395bcfaedb6fcaa58949e9cab095c220443af
Shaswato Sarker <61935359+TheCatWalk@users.noreply.github.com>
2023-11-26 11:45:36 +0200

    2 new files for data collection and statistics

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Simulation parameters
-SIMULATION_TIME = 500  # Total simulation time
+SIMULATION_TIME = 1000  # Total simulation time
 P_TIME_RANGE = (5, 15)  # Range of time for Preparation phase
 OT_TIME_RANGE = (10, 30)  # Range of time for Operation Theater phase
 R_TIME_RANGE = (5, 20)  # Range of time for Recovery phase
@@ -9,7 +9,7 @@ QUEUE_BATCH_SIZE = 5  # Number of patients per batch
 BATCH_INTERVAL = 0  # After which new batch is generated
 
 # Resource capacities
-P_CAPACITY = 1  # Number of parallel preparation rooms
+P_CAPACITY = 2  # Number of parallel preparation rooms
 OT_CAPACITY = 1  # Number of operation theaters
 R_CAPACITY = 100000  # Using a very high number to simulate 'infinite' capacity
 
